3117b3a2f3e6dd08668301639980d56a814fe0ec
anastaysia94-sudo <anastaysia94@gmail.com>
2026-09-06 02:48:23 -0700

    Fix Android Gradle project name for v10 build

--- android-fsa/settings.gradle.kts
+++ android-fsa/settings.gradle.kts
@@ -1,4 +1,4 @@
 pluginManagement { repositories { google(); mavenCentral(); gradlePluginPortal() } }
 dependencyResolutionManagement { repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS); repositories { google(); mavenCentral() } }
-rootProject.name = "F.S.A."
+rootProject.name = "FishShooterArcade"
 include(":app")
